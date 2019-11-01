1dda786d3b2478f9ae9e75b6439858b2a95d49a3
kenolane <kenolane28@gmail.com>
2019-11-01 15:54:56 +0100

    fix du controller frontend

--- src/Controller/Frontend.php
+++ src/Controller/Frontend.php
@@ -6,12 +6,4 @@ public function userConnect()
 {
     $userManager = new UserManager();
     $user_connect = $userManager->userConnect();
-    if ($resultat == false)
-    {
-        throw new \Exception('Mauvais mot de passe');
-    }
-    else
-    {
-        header('location:index.php?access=home');
-    }
 }
