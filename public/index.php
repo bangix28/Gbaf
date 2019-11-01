bde75049ff44442a80573457d60a7f6d1b6156ef
kenolane <kenolane28@gmail.com>
2019-11-01 14:17:32 +0100

    fix de l'index  et du router

--- public/index.php
+++ public/index.php
@@ -1,25 +1,13 @@
 <?php
-require '../vendor/autoload.php';
-use App\Controller;
-try
-{
-if (isset($_GET['access']))
-{
-    if ($_GET['access'] == 'userConnect')
-    {
-        userConnect();
-    }
-    elseif($_GET['access'] == 'home')
-    {
-        if ($_SESSION == true);
-        {
-            require('../src/View/Frontend/homeView.php');
-        }
-    }
-}
-}
-catch (Exception $e)
-{
-    echo 'Erreur:' . $e->getMessage();
-    require('ErrowView.php');
-}
+/* Get the necessary elements by using namespaces (uncomment Tracy to get the Debugger) */
+use App\Router;
+// use Tracy\Debugger;
+/* Required call to load the classes with the Composer Autoload */
+require_once '../vendor/autoload.php';
+/* Create the router */
+$router = new Router();
+/* Test Zone (uncomment Debugger to get Tracy & var_dump to get router object) */
+// Debugger::enable();
+// var_dump($router);
+/* Run application */
+$router->run();
