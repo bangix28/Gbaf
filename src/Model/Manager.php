689bb382e1ddafcdf348820af1234005ea0ea129
granger kenolane <kenolane28@gmail.com>
2019-11-05 15:11:17 +0100

    fix du config db

--- src/Model/Manager.php
+++ src/Model/Manager.php
@@ -10,10 +10,8 @@ class Manager
 {
     public function __construct()
     {
-        $db_dsn = DB_DSN;
-        $db_user = DB_USER;
-        $db_pass = DB_PASS;
-        $bd = new PDO('mysql:host=localhost;dbname=gbaf;charset=utf8', 'root', '');
-        return $bd;
+
+        $bd = new PDO(DB_DSN, DB_PASS, DB_USER, );
+        require '../config/db.php';
     }
 }
