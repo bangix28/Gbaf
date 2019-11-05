ac96cd620fbeb0c929b134d44d52d288e4f4ad7d
granger kenolane <kenolane28@gmail.com>
2019-11-05 15:28:32 +0100

    Fix du content

--- src/View/Frontend/userConnectView.php
+++ src/View/Frontend/userConnectView.php
@@ -1,22 +1,21 @@
 <?php $title = 'Connexion au site du GBAF'; ?>
-<?php ob_start(); ?>
 
+<?php ob_start(); ?>
 <form method="POST" action="../../../public/index.php?access=userConnect">
-    <fieldset>
-        <legend>Legend</legend>
         <div class="form-group row">
-        <div class="form-group">
+        <div class="form-group offset-4 col-5 text-center">
             <label for="username">Pseudo</label>
             <input type="text" class="form-control" id="username" placeholder="Pseudo">
         </div>
-        <div class="form-group">
-            <label for="password1">Password</label>
-            <input type="password" class="form-control" id="password1" placeholder="Password">
+        <div class="form-group offset-4 col-5 text-center">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" placeholder="Password">
         </div>
+            <div class="row">
         <button type="submit" class="btn btn-primary">Envoyer</button>
+            </div>
         </div>
-    </fieldset>
 </form>
-<? $content = ob_get_clean(); ?>
-<?php require'template.php'; ?>
+<?php $content = ob_get_clean(); ?>
+<?php require ('template.php'); ?>
 
