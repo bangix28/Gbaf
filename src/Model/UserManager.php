f11a8af0cfd97d3c528716c855209d2c7b6735a4
granger kenolane <kenolane28@gmail.com>
2019-11-06 17:13:57 +0100

    Add testUsername

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -1,13 +1,22 @@
 <?php
 namespace App\Model;
-use App\Model\Manager;
 use Exception;
 
 class UserManager extends Manager
 {
+    public function testUsername()
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT EXISTS(SELECT * FROM user WHERE username = ?) AS username_exist');
+        $req->execute(array($_POST['username']));
+        $user = $req->fetchColumn();
+        return $user;
+    }
     public function userRegister()
     {
         $db = $this->dbConnect();
+        $user = $db->prepare('INSERT INTO user(username, password, user_name, lastname, question , asnwer ) VALUE (?,?,?,?,?,?)');
+        $user->execute(array($_POST['username'],$_POST['password'],$_POST['user_name'],$_POST['lastname'],$_POST['question'],$_POST['answer']));
 
     }
     public function userConnect()
