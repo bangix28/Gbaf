d1616ef6db4ef646a3bd8a1448ce03c756444223
granger kenolane <kenolane28@gmail.com>
2019-11-06 17:13:23 +0100

    fix du name pour mysql

--- src/Router.php
+++ src/Router.php
@@ -20,11 +20,27 @@ class Router
                 {
                     if ($_GET['access'] == 'connect' )
                     {
-                        Controller\Frontend::userConnect();
+                        if(!empty($_POST['username']) AND !empty($_POST['password']))
+                        {
+                            Controller\Frontend::connect();
+                        }
                     }
                     elseif ($_GET['access'] == 'home')
                     {
                         require('View/Frontend/homeView.php');
+                    }
+                    elseif ($_GET['access'] == 'registerView')
+                    {
+                        require ('View/Frontend/userRegisterView.php');
+                    }
+                    elseif ($_GET['access'] == 'register')
+                    {
+                        if (!empty($_POST['username']) AND !empty($_POST['user_name']) AND  !empty($_POST['lastname']) AND !empty($_POST['password']) AND !empty($_POST['question']) AND !empty($_POST['answer']))
+                        {
+                         Controller\Frontend::register();
+                        }else{
+                            throw new Exception('vous n\'avez pas remplis tout le champs');
+                        }
                     }
                 }else{
                     require ('View/Frontend/userConnectView.php');
