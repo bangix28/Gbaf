f78e9e5ed01536a64882ddcff20be320b5e1dec1
granger kenolane <kenolane28@gmail.com>
2019-11-08 20:32:09 +0100

    Ajout de commentaire et getActor et addCreator

--- src/Controller/ActorController.php
+++ src/Controller/ActorController.php
@@ -3,12 +3,32 @@
 namespace App\Controller;
 use App\Model\ActorManager;
 
+/**
+ * Class ActorController
+ * @package App\Controller
+ */
 class ActorController
 {
+    /**
+     * @var ActorManager|null
+     */
     private $actorManager = null;
 
+    /**
+     * ActorController constructor.
+     */
     public function __construct()
     {
         $this->actorManager = new ActorManager();
+    }
+
+    public function listActor()
+    {
+       $this->actorManager->getActors();
+    }
+
+    public function addCreator()
+    {
+        $this->actorManager->addCreator();
     }
 }
