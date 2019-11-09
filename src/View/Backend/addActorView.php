47db15c93f5baf65c22c32d83351a5ce3721adba
granger kenolane <kenolane28@gmail.com>
2019-11-09 16:21:43 +0100

    fix du bootstrap et  change de title a link

--- src/View/Backend/addActorView.php
+++ src/View/Backend/addActorView.php
@@ -1,25 +1,31 @@
 <?php $title = 'Ajouter un acteur/partenaire !'; ?>
 <?php ob_start()    ?>
-<section class="col-12">
-    <form method="POST" enctype="multipart/form-data" class="text-center">
-        <div class="form-group">
-            <label for="name">Nom de l'acteur/partenaire</label>
-            <input type="text" required class="form-control" name="name">
-        </div>
-        <div class="form-control">
-            <label for="logo">Logo</label>
-            <input type="file" required name="logo" class="form-control" placeholder="Ajouter un logo ici !" />
-        </div>
-        <div class="form-control">
-            <label for="title">Titre de votre déscription</label>
-            <input type="text" required class="form-control" name="title">
-        </div>
-        <div class="form-control">
-            <label for="description">description de l'acteur/partenaire</label>
-            <input type="text" required class="form-control" name="description">
-        </div>
-        <button type="submit">Valider !</button>
-    </form>
+    <h1 class="lead text-center"> Creation d'un acteur/partenaire !</h1>
+<section class="container-fluid h-100  text-dark">
+    <div class="row justify-content-center align-items-center">
+        <form method="POST" enctype="multipart/form-data" class="text-center">
+            <div class="form-group">
+                <label  class="control-label " for="name">Nom de l'acteur/partenaire</label >
+                <input type="text" required class="form-control" name="name">
+            </div>
+            <div class="form-group">
+                <label class="control-label" for="logo">Logo</label >
+                <input type="file" required name="logo" class="form-control-file" placeholder="Ajouter un logo ici !" />
+            </div>
+            <div class="form-group">
+                <label class="control-label"  for="link">Lien du site de votre banque !</label >
+                <input type="text" required class="form-control" name="link">
+            </div>
+            <div class="form-group">
+                <label class="control-label"  for="description">description de l'acteur/partenaire</label >
+                <textarea required class="form-control" name="description" ></textarea>
+            </div>
+            <button type="submit">Valider !</button>
+            <div class="text-center"
+                <p class="col-10"><?php if (isset($message)) { echo $message; } ?></p>
+            </div>
+        </form>
+    </div>
 </section>
 <?php $content = ob_get_clean() ?>
-<?php require '../Frontend/template.php'; ?>
+<?php require 'template.php'; ?>
