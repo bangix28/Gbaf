51d1e9df4b569be871a195e0035258d5f747954d
granger kenolane <kenolane28@gmail.com>
2019-11-11 18:50:03 +0100

    changement de place du required vendor

--- public/index.php
+++ public/index.php
@@ -2,11 +2,11 @@
 /* Get the necessary elements by using namespaces (uncomment Tracy to get the Debugger) */
 use App\Router;
 use Tracy\Debugger;
+require_once '../vendor/autoload.php';
 /* Required call to load the classes with the Composer Autoload */
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-require_once '../vendor/autoload.php';
 /* Create the router */
 $router = new Router();
 /* Test Zone (uncomment Debugger to get Tracy & var_dump to get router object) */
