457d44e97d7d075438cd6758460374101ec43f54
granger kenolane <kenolane28@gmail.com>
2019-11-16 11:51:11 +0100

    Ajout

--- config/gbaf.sql
+++ config/gbaf.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  mar. 29 oct. 2019 à 16:06
+-- Généré le :  jeu. 14 nov. 2019 à 20:13
 -- Version du serveur :  10.4.8-MariaDB
--- Version de PHP :  7.3.10
+-- Version de PHP :  7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,32 +25,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `actor`
+--
+
+CREATE TABLE `actor` (
+  `actor_id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `logo` text NOT NULL,
+  `tiny_desc` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `actor_like` int(11) NOT NULL,
+  `actor_dislike` int(11) NOT NULL,
+  `link` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `actor`
+--
+
+INSERT INTO `actor` (`actor_id`, `name`, `logo`, `tiny_desc`, `description`, `actor_like`, `actor_dislike`, `link`) VALUES
+(1, 'Formation&co ', 'formation_co.png', 'Formation&co est une association française présente sur tout le territoire.\r\nNous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit et un accompagnement profess', 'Formation&co est une association française présente sur tout le territoire.', 0, 0, 'Formation&co.fr'),
+(2, 'Protectpeople', 'protectpeople.png', 'Chez Protectpeople, chacun cotise selon ses moyens et reçoit selon ses besoins.\r\nProectecpeople est ouvert à tous, sans considération d’âge ou d’état de santé.\r\nNous garantissons un accès aux soins et', 'Protectpeople finance la solidarité nationale.\r\nNous appliquons le principe édifié par la Sécurité sociale française en 1945 : permettre à chacun de bénéficier d’une protection sociale.', 0, 0, 'Protectpeople.fr'),
+(3, 'Dsa France', 'Dsa_france.png', 'Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales.\r\nNous accompagnons les entreprises dans les étapes clés de leur évolution.\r\nNotre philosophie : s’adap', 'Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales.\r\nNous accompagnons les entreprises dans les étapes clés de leur évolution.\r\nNotre philosophie : s’adapte', 0, 0, 'DsaFrance.fr'),
+(4, 'La CDE', 'CDE.png', 'La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation. \r\nSon président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.', 'La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation. \r\nSon président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.', 0, 0, 'CDE.fr');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `comment`
 --
 
 CREATE TABLE `comment` (
   `id` int(11) NOT NULL,
-  `id_partner` int(11) NOT NULL,
+  `actor_id` int(11) NOT NULL,
   `comment_date` datetime NOT NULL,
   `author` text NOT NULL,
   `comment` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Structure de la table `partner`
+-- Déchargement des données de la table `comment`
 --
 
-CREATE TABLE `partner` (
-  `partner_id` int(11) NOT NULL,
-  `name` text NOT NULL,
-  `logo` text NOT NULL,
-  `title` text NOT NULL,
-  `description` text NOT NULL,
-  `parner_like` int(11) NOT NULL,
-  `partner_dislike` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `comment` (`id`, `actor_id`, `comment_date`, `author`, `comment`) VALUES
+(1, 4, '2019-11-14 13:57:11', 'kenolane', 'super banque !'),
+(2, 4, '2019-11-14 13:57:36', 'kenolane', 'quelle sont les taux d’intérêt ?'),
+(4, 3, '2019-11-14 18:52:26', 'kenolane', 'test');
 
 -- --------------------------------------------------------
 
@@ -64,25 +84,33 @@ CREATE TABLE `user` (
   `password` text NOT NULL,
   `name` text NOT NULL,
   `lastname` text NOT NULL,
-  `ask_secret` int(11) NOT NULL,
-  `answer` text NOT NULL
+  `question` int(11) NOT NULL,
+  `answer` text NOT NULL,
+  `image` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `name`, `lastname`, `question`, `answer`, `image`) VALUES
+(1, 'kenolane', '$2y$10$e2P8TkIlh0rbej7.Ua8Bo.60lgR3WDwdI5mZEJ.qsPTvvvnN7M5H2', 'granger', 'kénolane', 1, 'kenolane', 'utilisateur.png');
 
 --
 -- Index pour les tables déchargées
 --
 
 --
+-- Index pour la table `actor`
+--
+ALTER TABLE `actor`
+  ADD PRIMARY KEY (`actor_id`);
+
+--
 -- Index pour la table `comment`
 --
 ALTER TABLE `comment`
   ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `partner`
---
-ALTER TABLE `partner`
-  ADD PRIMARY KEY (`partner_id`);
 
 --
 -- Index pour la table `user`
@@ -95,22 +123,22 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT pour la table `actor`
+--
+ALTER TABLE `actor`
+  MODIFY `actor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT pour la table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `partner`
---
-ALTER TABLE `partner`
-  MODIFY `partner_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
